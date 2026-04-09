077f584f98cdab61789db6ef54a1d27d430764a6
gauravKothari <kotharigaurav6@gmail.com>
2026-04-09 18:35:51 +0530

    js

---
+++ Javascript/1 Introduction_Basic Programs/myScript.js
@@ -0,0 +1,3 @@
+document.write("This is my First javaScript example<br>")
+document.write('<span style="color:red">This is my First javaScript example</span><br>') 
+document.write("This is my First javaScript example<br>") 
